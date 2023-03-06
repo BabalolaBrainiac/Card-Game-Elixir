dfcf020d2a903c8bd2361e1cf946a8f173aa88ab
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-03-06 20:38:28 +0100

    Initial commit

---
+++ lib/cards.ex
@@ -0,0 +1,22 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def helloWorld do
+    "hi there"
+  end
+end
