d96fe78d22d41f2a56306b3987d7cacb156fe238
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-03-11 15:31:57 +0100

    updated

--- lib/cards.ex
+++ lib/cards.ex
@@ -24,16 +24,16 @@ defmodule Cards do
     ["Ace", "Spades", "King"]
     values = ["Ace", "Spades", "King"]
     suits = ["Spades", "Clubs", "Diamonds", "Random"]
-    
+
     for suit <- suits, value <- values  do
       "#{value} of #{suit}"
     end
   end
-  
+
   def shuffle(card_deck) do
     Enum.shuffle(card_deck)
   end
-  
+
  def if_contains?(deck, card) do
    Enum.member?(deck, card)
  end
@@ -44,7 +44,8 @@ defmodule Cards do
  end
 
  def save(deck, filename) do
-   to_binary = :erlang.term_to_binary
-
+   to_binary = :erlang.term_to_binary(deck)
+   File.write(filename, to_binary)
  end
+
 end
